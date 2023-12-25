5b1cd59fa04ffff233daa6107dc679ff0523e8bf
abdallah-hatem <ahkortam@gmail.com>
2023-12-25 21:18:07 +0200

    Add project files.

---
+++ WebApiTest/Model/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiTest.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public required string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        public required string Password { get; set; }
+    }
+}
